01e31022dc6ccc47bacab0dce74a6afa2c40b1d4
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-01-09 22:25:56 -0800

    Create OccurrencesHashMap.java
    Find the number of occurrences of a name using Hashmap

---
+++ src/ex1/OccurrencesHashMap.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurrencesHashMap {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> hMap = new HashMap<>();
+		String [] names = {"Atyab", "Fatima", "Saeed", "Malak", "Fatima", "Daniyal", "Abdullah", "Abdullah"};
+		for(String name : names) {
+			if(hMap.containsKey(name)) {
+				hMap.put(name ,hMap.get(name)+1);
+			}
+			else {
+				hMap.put(name,1);
+			}
+		}
+
+		for(Map.Entry<String, Integer> elemants : hMap.entrySet()) {
+			System.out.println(elemants.getKey() + " is repeated " + elemants.getValue()+" Times");
+		}
+	}
+
+}
