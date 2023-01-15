7af3f587d9b012c63faec31c05a58b7ae3910da0
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-01-14 21:57:46 -0800

    find the second highest number in the array.

---
+++ src/tasks/TaskfindSecondHighest.java
@@ -0,0 +1,21 @@
+package tasks;
+
+import java.util.Arrays;
+
+public class TaskfindSecondHighest {
+	 
+	//Task1: given an array of integers, find the second highest number in the array.
+public static int findSecondHighest(int [] numbers) {
+		int numberOfElments = numbers.length ;
+		Arrays.sort(numbers);
+		
+		return numbers[numberOfElments - 2];
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		int [] numbers = {50,20,12,2,3,5,10,2};
+		System.out.println("Second Highest number is " + findSecondHighest(numbers));
+	}
+}
